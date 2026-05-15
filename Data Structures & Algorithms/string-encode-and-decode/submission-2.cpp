63c657b1e7bab4ee04d0866f4705b50fc5246433
SHARVESH PRABHU <215662336+sharveshprabhu@users.noreply.github.com>
2026-05-15 17:25:22 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string coded;
+        for(const string &str:strs){
+            int n=str.length();
+            coded=coded+to_string(n)+"#"+str;
+        }
+        return coded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res;
+        bool flag=false;
+        int i=0;
+        while(i<s.size()){
+            int x = s.find("#",i);
+            int len = stoi(s.substr(i,x-i));
+            string word = s.substr(x+1,len);
+            i = x+len+1;
+            res.push_back(word);
+        }
+        return res;
+    }
+};
